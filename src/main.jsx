fe019f6b51587f3403b54aea1378244d0734cebf
DuongTai0106 <duongthanhtai0106@gmail.com>
2024-10-23 15:58:14 +0700

    finish register page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/global.css";
+import RegisterPage from "./pages/register.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import UserPage from "./pages/user.jsx";
+import HomePage from "./pages/home.jsx"
+import LoginPage from "./pages/login.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "user",
+        element: <UserPage />,
+      },
+    ],
+  },
+
+  {
+    path: "register",
+    element: <RegisterPage />,
+  },
+
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
